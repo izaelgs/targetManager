4866c4377a44cad878907bb259ba8f7a007a387e
izaelgs <izael-123@outlook.com>
2022-11-15 20:29:19 -0300

    exibicao de etapas e mixin de exibicao de toasts

---
+++ resources/js/mixins/appendToast.vue
@@ -0,0 +1,29 @@
+<script>
+export default{methods: {
+    appendToast(mensagem, status) {
+        return new Promise((resolve, reject) => {
+            let toast = document.createElement("div");
+
+            toast.classList.add("toast");
+            toast.id = "liveToast";
+            toast.role = "alert";
+            toast.setAttribute("aria-live", "assertive");
+            toast.setAttribute("aria-atomic", "true");
+
+            toast.innerHTML = `
+            <div class="toast-header  bg-${status} text-light">
+                <strong class="me-auto">TargetManager</strong>
+                <small>Agora</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                ${mensagem}
+            </div>`;
+
+            document.querySelector(".toast-container").append(toast);
+
+            resolve(toast);
+        });
+    }
+}}
+</script>
--- resources/js/pages/Target.vue
+++ resources/js/pages/Target.vue
@@ -30,17 +30,112 @@
             </span>
         </div>
         <p>{{ target.description }}</p>
+        <div class="list-group text-start">
+            <form
+                v-for="stage in editableStages"
+                @submit.stop.prevent="submitEdit(stage)"
+                class="list-group-item card bg-secondary text-light my-2 d-flex flex-row align-items-center"
+            >
+                <input
+                    class="form-check-input me-1 pointer"
+                    type="checkbox"
+                    value=""
+                />
+                <div class="vr me-2"></div>
+                <div class="w-100">
+                    <div class="d-flex w-100 justify-content-between">
+                        <input
+                            v-show="stage.edit"
+                            v-model="stage.title"
+                            type="text"
+                            name="title"
+                            class="form-control"
+                        />
+                        <h5 v-show="!stage.edit" class="mb-1">
+                            {{ stage.title }}
+                            <button
+                                @click.prevent="edit_stage(stage)"
+                                class="btn text-light px-2"
+                            >
+                                <i class="bi bi-pencil"></i>
+                            </button>
+                            <button
+                                @click.prevent="delete_stage(stage)"
+                                class="btn text-light px-2"
+                            >
+                                <i class="bi bi-trash"></i>
+                            </button>
+                        </h5>
+                        <small v-show="!stage.edit">{{ stage.deadline }}</small>
+                        <button
+                            v-show="stage.edit"
+                            @click.prevent="edit_stage(stage)"
+                            class="btn text-light px-2"
+                        >
+                            <i class="bi bi-x-lg"></i>
+                        </button>
+                    </div>
+                    <div v-show="stage.edit" class="row">
+                        <div class="col-12">
+                            <textarea
+                                v-show="stage.edit"
+                                v-model="stage.description"
+                                name="description"
+                                class="form-control my-2"
+                                cols="30"
+                                rows="5"
+                            ></textarea>
+                        </div>
+                        <div class="col">
+                            <input
+                                v-model="stage.deadline"
+                                type="date"
+                                class="form-control my-1"
+                            />
+                        </div>
+                        <div class="col-6">
+                            <input
+                                v-model="stage.complexity"
+                                type="number"
+                                class="form-control my-1"
+                            />
+                        </div>
+                        <div class="col-6">
+                            <input
+                                v-model="stage.tolerance"
+                                type="number"
+                                class="form-control my-1"
+                            />
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-primary" type="submit">
+                                Salvar
+                            </button>
+                        </div>
+                    </div>
+                    <span v-show="!stage.edit" class="badge text-bg-primary">
+                        Complexidade: {{ stage.complexity }}
+                    </span>
+                    <span v-show="!stage.edit" class="badge text-bg-primary">
+                        Tolerância: {{ stage.tolerance }}
+                    </span>
+                    <br />
+                    <small v-show="!stage.edit">{{ stage.description }}</small>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
 import Cookie from "js-cookie";
+import appendToast from "../mixins/appendToast.vue";
 
 export default {
     data() {
         return {
             target: {},
-            token: "",
+            access_token: "",
         };
     },
 
@@ -48,27 +143,107 @@ export default {
         this.$watch(
             () => this.$route.params,
             (toParams, previousParams) => {
-                this.getContent();
+                this.fetchData();
             }
         );
-        this.token = Cookie.get("access_token");
+        this.access_token = Cookie.get("access_token");
 
-        this.getContent();
+        this.fetchData();
+    },
+
+    computed: {
+        editableStages() {
+            console.log(this.target.stages);
+            return this.target.stages;
+        },
     },
 
     methods: {
-        getContent() {
-            fetch("http://localhost:8000/api/target/" + this.$route.params.id, {
-                method: "GET",
+        edit_stage(stage) {
+            stage.edit = !stage.edit;
+        },
+
+        submitEdit(stage) {
+            const payload = {
+                title: stage.title,
+                description: stage.description,
+                target_id: stage.target_id,
+                tolerance: stage.tolerance,
+                deadline: stage.deadline,
+                complexity: stage.complexity,
+                stageid: stage.stageid,
+            };
+
+            fetch("http://127.0.0.1:8000/api/stage/" + stage.id, {
+                method: "PUT",
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
-                    Authorization: "Bearer " + this.token,
+                    Authorization: "Bearer " + this.access_token,
+                },
+                body: JSON.stringify(payload),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (!data.error) {
+                        this.appendToast(data.message, "success").then(
+                            (element) => {
+                                const toast = new bootstrap.Toast(element);
+                                toast.show();
+                            }
+                        );
+
+                        this.fetchData();
+                    } else {
+                        this.appendToast(data.error, "danger").then(
+                            (element) => {
+                                const toast = new bootstrap.Toast(element);
+                                toast.show();
+                            }
+                        );
+                    }
+                });
+        },
+
+        delete_stage(stage) {
+            fetch("http://localhost:8000/api/stage/" + stage.id, {
+                method: "DELETE",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + this.access_token,
                 },
             })
                 .then((response) => response.json())
                 .then((data) => {
                     if (!data.error) {
+                        const index = this.target.stages.indexOf(stage);
+                        if (index > -1) {
+                            // only splice array when item is found
+                            this.target.stages.splice(index, 1); // 2nd parameter means remove one item only
+                        }
+                    } else {
+                        alert(data.error);
+                    }
+                });
+        },
+
+        fetchData() {
+            fetch("http://localhost:8000/api/target/" + this.$route.params.id, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + this.access_token,
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (!data.error) {
+                        data.stages = data.stages.map((stage) => {
+                            stage.edit = false;
+                            return stage;
+                        });
                         this.target = data;
                     } else {
                         alert(data.error);
@@ -76,5 +251,7 @@ export default {
                 });
         },
     },
+
+    mixins: [appendToast]
 };
 </script>
